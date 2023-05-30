31f44e5d970a4357bfa78b6710dbe398f6c81633
AhsanZeya <ahsanzeya10@gmail.com>
2023-05-30 17:01:17 +0530

    new page for add multiple contact for single user

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -1,13 +1,25 @@
 package com.smart.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 
 @Controller
@@ -17,9 +29,10 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@RequestMapping("/index")
-	public String dashboard(Model model,Principal principal) {
-		
+	
+//	method for adding common data to response
+	@ModelAttribute
+	public void addCommonData(Model model,Principal principal) {
 		
 		String userName = principal.getName();
 		System.out.println("USERNAME " +userName);
@@ -32,10 +45,84 @@ public class UserController {
 		
 		System.out.println("USER  " +user);
 		
-		
-		
-		
+	}
+	
+	
+//	Dashboard home
+	@RequestMapping("/index")
+	public String dashboard(Model model,Principal principal) {
+   
+		model.addAttribute("title", "User Dashboard");
 		return "normal/user_dashboard";
 	}
+	
+	
+	
+	//open add form handler	
+	@GetMapping("/add-contact")
+	public String openAddContactForm(Model model) {
+		model.addAttribute("title", "Add Contact");
+		model.addAttribute("contact", new Contact());
+		return "normal/add_contact_form";
+	}
 
+	
+//	processing add contact form
+	@PostMapping("/process-contact")
+	public String processContact(
+			@ModelAttribute Contact contact,
+			@RequestParam("profileImage") MultipartFile file ,
+			Principal principal) {
+		
+		try {
+			String userName = principal.getName();
+			User user = userRepository.getUserByUserName(userName);
+			
+			//processing and uploading file...
+			
+			if(file.isEmpty()) {
+				//if the file is empty then try our message
+				
+				System.out.println("File is empty");
+			}
+			
+			else {
+				//file = file to folder and update the name to contact
+				
+				contact.setImage(file.getOriginalFilename());
+				
+				File saveFile = new ClassPathResource("/static/img").getFile();
+				
+				Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+				
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				
+				System.out.println("Image is uploaded"+" "+File.separator+"   "+saveFile.getAbsolutePath());
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			contact.setUser(user); //It help to get the id of user in contact ..  to make more then one contacts for single user.
+			
+			user.getContacts().add(contact);
+			
+			this.userRepository.save(user);
+			
+			System.out.println("DATA "+contact);
+			
+			System.out.println("Added to database");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("ERROR "+e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return "normal/add_contact_form";
+	}
 }
--- smartcontactmanager/src/main/resources/static/js/script.js
+++ smartcontactmanager/src/main/resources/static/js/script.js
@@ -1,1 +1,21 @@
 console.log("this is script file")
+
+const toggleSidebar=()=>{
+
+    if($(".sidebar").is(":visible")){
+        //true
+        //band karna hai
+        $(".sidebar").css("display","none");
+        $(".content").css("margin-left","0%");
+        console.log("Band Krna hai")
+
+    }
+
+    else{
+        //false
+        //show krna hai
+        $(".sidebar").css("display","block");
+        $(".content").css("margin-left","20%");
+        console.log("show krna hai")
+    }
+};
